fb777b53be7e80c34be83ff26e2fd266f7ea92cd
amelia <ameliag_07@outlook.com>
2025-08-19 00:30:56 +0100

    P

---
+++ problem_4.py
@@ -0,0 +1,19 @@
+
+# Project Euler problem 4
+
+number = 9999
+if str(number) == str(number)[::-1]:  # Checks if the string follows palindromic logic
+    print("Palindromic")
+else:
+    print("Not palindromic")
+
+palindromes = [] # Created list to store possible products
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i * j
+        if str(product) == str(product)[::-1]: # Checks if the product is a palindrome
+            palindromes.append(product)
+
+largest_palindrome = max(palindromes) # Finds largest value in list
+
+print(largest_palindrome)
--- python/problem_3.py
+++ python/problem_3.py
@@ -1,5 +1,8 @@
 
 # Project Euler problem 3
 
-from sympy.ntheory import primefactors
-primefactors(600851475143)
+from sympy.ntheory import primefactors # Using primefactors function from sympy library to find prime factors
+
+ans = primefactors(600851475143)
+
+print(ans)
